b11bfc94a3a50eb8b25de588bb55e61e87ba6368
Robert Baluta <robert.baluta@hotmail.com>
2022-09-08 12:58:39 +0200

    fixed no. of errors bug

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -20,7 +20,7 @@ class GameController:
     def start(self, name: str, mode: str, minutes: int) -> None:
         word_decode = WordDecode()
         word_decode.read()
-        game_factory = GameFactory(Player(name), string.ascii_lowercase + 'üöä', 6, mode, minutes)
+        game_factory = GameFactory(Player(name), string.ascii_lowercase + 'üöä', 5, mode, minutes)
         
         self.game = game_factory.make_game()
         self.savePlayerNameToCSV(name)
